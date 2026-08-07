54fce6b3c0119f164b4784766a7c5880acccbc99
Douglas Silva <douglasitpro@gmail.com>
2026-08-06 23:27:16 -0300

    feat(exception): Cria a exceção customizada CepNotFoundException herdando de RuntimeException. Ela será lançada pelo ViaCepClient ou ViaCepService quando a API do ViaCEP retornar que o CEP consultado não existe no banco de dados deles.

---
+++ src/main/java/com/douglas/hub/exception/CepNotFoundException.java
@@ -0,0 +1,7 @@
+package com.douglas.hub.exception;
+
+public class CepNotFoundException extends RuntimeException {
+    public CepNotFoundException(String message) {
+        super(message);
+    }
+}
