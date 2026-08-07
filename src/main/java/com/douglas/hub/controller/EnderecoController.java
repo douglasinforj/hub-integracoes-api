0010c863579e2f80286ddd5916b5f7ae4853a917
Douglas Silva <douglasitpro@gmail.com>
2026-08-06 23:56:12 -0300

    feat(controller):Cria a classe EnderecoController mapeando o endpoint '/enderecos/{cep}'. Utiliza injeção de dependência via construtor para expor a lógica de busca de endereços a partir do ViaCepClient, retornando os dados encapsulados em um EnderecoDTO.

---
+++ src/main/java/com/douglas/hub/controller/EnderecoController.java
@@ -0,0 +1,27 @@
+package com.douglas.hub.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.douglas.hub.client.ViaCepClient;
+import com.douglas.hub.dto.EnderecoDTO;
+
+@RestController
+@RequestMapping("/enderecos")
+public class EnderecoController {
+
+    private final ViaCepClient viaCepClient;   //Injeta ViaCliente (dados da api externa)
+
+    public EnderecoController(ViaCepClient viaCepClient) {   // DI - Via construtor
+        this.viaCepClient = viaCepClient;
+    }
+
+    @GetMapping("/{cep}")
+    public ResponseEntity<EnderecoDTO>buscarEndereco(@PathVariable String cep) {
+        EnderecoDTO endereco = viaCepClient.buscarEndereco(cep);
+        return ResponseEntity.ok(endereco);
+    }
+}
