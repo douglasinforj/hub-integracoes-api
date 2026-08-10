fc5e127c6fcb6293195c9339b4353ba05af0f4c6
Douglas Silva <douglasitpro@gmail.com>
2026-08-10 19:03:31 -0300

    feat(weather): adiciona integração com API OpenWeatherMap via WebClient - Integra com a API OpenWeatherMap utilizando WebClient reativo - Mapeia parâmentros para idioma pt_br e unidade metrica - Trata erros HTTP 404 (cidade não encontrada) e 5xx (servidor indisponivel)

---
+++ src/main/java/com/douglas/hub/client/WeatherClient.java
@@ -0,0 +1,48 @@
+
+package com.douglas.hub.client;
+
+import com.douglas.hub.dto.weather.ClimaDTO;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+@Component
+public class WeatherClient {
+
+    private final WebClient weatherWebClient;
+
+    @Value("${weather.api-key}")
+    private String apiKey;
+
+    public WeatherClient(WebClient weatherWebClient) {
+        this.weatherWebClient = weatherWebClient;
+    }
+
+    public Mono<ClimaDTO> buscarClima(String cidade) {
+        return weatherWebClient
+                .get()                                  //prepara a requisção para utilizar método get
+                // path + query params
+                .uri(uri -> uri                         //COnstroi dinamicamente a URL da chamada
+                        .path("/weather")               //Define o endpoint base da Api
+                         //queryPAram: Insere os parâmentros na URL
+                        .queryParam("q", cidade)         //q com nome da cidade
+                        .queryParam("appid", apiKey)     // appi com a chave de acesso
+                        .queryParam("units", "metric")   // Celsius
+                        .queryParam("lang", "pt_br")     // descrição em português
+                        .build())
+                .retrieve()                             //Dispara a requisição para obter a resposta
+                // onStatus: trata erros HTTP de forma reativa, antes do corpo da resposta
+                .onStatus(
+                        status -> status.value() == 404,    //Interromp o fluxo reativo e lança erro customizado
+                        response -> Mono.error(new RuntimeException("Cidade não encontrada: " + cidade))
+                )
+                .onStatus(
+                        status -> status.is5xxServerError(),  // Se o servidor API falhar lança exceção informando indisponibilidade
+                        response -> Mono.error(new RuntimeException("OpenWeatherMap fora do ar"))
+                )
+                .bodyToMono(ClimaDTO.class); // deserializa o JSON para o DTO. 
+                // Mapeia e desserializa o payload JSON retornado pela API 
+                // diretamente para um objeto Java do tipo ClimaDTO.
+    }
+}
