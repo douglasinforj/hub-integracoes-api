d73297ce15bf66fa93f65ef9935f810e37688a83
Douglas Silva <douglasitpro@gmail.com>
2026-08-11 11:02:05 -0300

    feat(hub): habilitar o OpenFeign na Aplicação

--- src/main/java/com/douglas/hub/HubApplication.java
+++ src/main/java/com/douglas/hub/HubApplication.java
@@ -2,8 +2,10 @@ package com.douglas.hub;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients                //Ativa o escaneamento de interfaces @FeignClient
 public class HubApplication {
 
     public static void main(String[] args) {
